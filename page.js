e14bd3ff19739bb0bedb4b2c226c20b666a4e7b4
Rick <vrick6749@gmail.com>
2021-12-24 19:20:23 +1100

    function is tested and fixed an error where it was returning undefined

--- page.js
+++ page.js
@@ -1,22 +1,25 @@
 const prompt = require('prompt-sync')();
 
-const arrayOfJokes = ['joke1', 'joke2','joke3','joke4','joke5'];
+const arrayOfJokes = ['joke 1', 'joke 2','joke 3','joke 4','joke 5'];
 let answer = 'yes';
+
+
 function getRandomJoke(){
-   let randomNumber = Math.floor((Math.random() * arrayOfJokes.length)+1)
-   return randomNumber
+   let randomNumber = Math.floor((Math.random() * arrayOfJokes.length))
+   let randomJoke = arrayOfJokes[randomNumber]
+   return randomJoke
 }
     
     while(answer != 'no'){
-        console.log('Tell joke')
+        
+        console.log(getRandomJoke())
 
-
-        answer = prompt('hear a joke? yes or no')
+        answer = prompt('hear a joke? yes or no ')
         
 
     }
 
-    
+
 
 
 
